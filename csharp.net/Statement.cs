901f18001f05eeb9120434f6e63ff01f8adc188b
ykaty honey <ykatyhoney@gmail.com>
2021-02-15 02:47:41 -0800

    add variables and functions express

---
+++ csharp.net/Statement.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace csharp.net
+{
+    public class Statement
+    {
+        static void Main(string[] args)
+        {
+            int number;
+
+            Console.WriteLine("please enter a number between 0 and 10");
+
+            number = int.Parse(Console.ReadLine());
+
+            if (number > 10)
+                Console.WriteLine("Hey! The number should be 10 or less!");
+
+            else
+            if (number < 0)
+                Console.WriteLine("Hey! The number should be 0 or more!");
+
+            else
+                Console.WriteLine("good job!");
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
--- csharp.net/variable-scope.cs
+++ csharp.net/variable-scope.cs
@@ -1,23 +1,23 @@
-﻿using System;
+﻿//using System;
 
-namespace csharp.net
-{
-    class Program
-    {
-        private static string helloClass = "Hello, class!";
+//namespace csharp.net
+//{
+//    class Program
+//    {
+//        private static string helloClass = "Hello, class!";
 
-        static void Main(string[] args)
-        {
-            string helloLocal = "hello. Local!";
-            Console.WriteLine(helloLocal);
-            Console.WriteLine(Program.helloClass);
-            DoStuff();
+//        static void Main(string[] args)
+//        {
+//            string helloLocal = "hello. Local!";
+//            Console.WriteLine(helloLocal);
+//            Console.WriteLine(Program.helloClass);
+//            DoStuff();
 
-        }
+//        }
 
-        static void DoStuff()
-        {
-            Console.WriteLine("A message from DoStuff:" + Program.helloClass);
-        }
-    }
-}
+//        static void DoStuff()
+//        {
+//            Console.WriteLine("A message from DoStuff:" + Program.helloClass);
+//        }
+//    }
+//}
