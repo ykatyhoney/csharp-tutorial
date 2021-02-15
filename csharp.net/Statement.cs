53f792221e926e3be12922dd5b59091b51a7b4e8
ykaty honey <ykatyhoney@gmail.com>
2021-02-15 09:17:53 -0800

    switch case

--- csharp.net/Statement.cs
+++ csharp.net/Statement.cs
@@ -6,28 +6,44 @@ namespace csharp.net
     {
         static void Main(string[] args)
         {
-            int number;
+            //int number;
 
-            Console.WriteLine("please enter a number between 0 and 10");
+            //Console.WriteLine("please enter a number between 0 and 10");
 
-            number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Do you enjoy C#?(yes/or/maybe)");
 
-            //if (number > 10)
-            //    Console.WriteLine("Hey! The number should be 10 or less!");
+            string input = Console.ReadLine();
 
+            switch (input)
+            {
+                case "yes":
+                case "maybe":
+                    Console.WriteLine("great");
+                    break;
+
+                case "No":
+                    Console.WriteLine("Too bad!");
+                    break;
+            }
+
+            //number = int.Parse(Console.ReadLine());
+
+            ////if (number > 10)
+            ////    Console.WriteLine("Hey! The number should be 10 or less!");
+
+            ////else
+            ////if (number < 0)
+            ////    Console.WriteLine("Hey! The number should be 0 or more!");
+
+            ////else
+            ////    Console.WriteLine("good job!");
+
+            //if (number > 10 || (number < 0))
+            //    Console.WriteLine("Hey! the number should be 0 or more and 10 or less!");
             //else
-            //if (number < 0)
-            //    Console.WriteLine("Hey! The number should be 0 or more!");
+            //    Console.WriteLine("Good job!");
 
-            //else
-            //    Console.WriteLine("good job!");
-
-            if (number > 10 || (number < 0))
-                Console.WriteLine("Hey! the number should be 0 or more and 10 or less!");
-            else
-                Console.WriteLine("Good job!");
-
-            Console.ReadLine();
+            //Console.ReadLine();
 
 
         }
