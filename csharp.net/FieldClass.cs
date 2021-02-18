78700b9f06934f2325dcb6c5d465e9dbbee7c935
ykaty honey <ykatyhoney@gmail.com>
2021-02-17 19:00:26 -0800

    this identifier

---
+++ csharp.net/FieldClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace csharp.net
+{
+    public class FieldClass
+    {
+        private string name = "John Doe";
+        private int age = 42;
+
+        public void Describe()
+        {
+            string description = this.name + "is" + this.age + "years old..";
+            Console.WriteLine(description);
+            
+        }
+     
+    }
+}
+
--- csharp.net/ObjectClass.cs
+++ csharp.net/ObjectClass.cs
@@ -1,43 +1,43 @@
-﻿using System;
+﻿//using System;
 
 
-namespace csharp.net
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Car car;
+//namespace csharp.net
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Car car;
 
-            car = new Car("Red");
-            Console.WriteLine(car.Describe());
+//            car = new Car("Red");
+//            Console.WriteLine(car.Describe());
 
-            car = new Car("Green");
-            Console.WriteLine(car.Describe());
+//            car = new Car("Green");
+//            Console.WriteLine(car.Describe());
 
-            Console.ReadLine();
+//            Console.ReadLine();
 
-        }
+//        }
 
-    class Car
-        {
-            private string color;
+//    class Car
+//        {
+//            private string color;
 
-            public Car(string color)
-            {
-                this.color = color;
-            }
+//            public Car(string color)
+//            {
+//                this.color = color;
+//            }
 
-            public string Describe()
-            {
-                return "This car is" + color;
-            }
+//            public string Describe()
+//            {
+//                return "This car is" + color;
+//            }
 
-            public string Color
-            {
-                get { return color; }
-                set { color = value; }
-            }
-        }
-    }
-}
+//            public string Color
+//            {
+//                get { return color; }
+//                set { color = value; }
+//            }
+//        }
+//    }
+//}
